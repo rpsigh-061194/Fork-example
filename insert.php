370e8fa0f60dca726e1df2205a34a5518bd5580f
rpsigh-061194 <73515923+rpsigh-061194@users.noreply.github.com>
2020-10-27 23:12:45 +0530

    Create insert.php
    data insert in database file

---
+++ insert.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "databasename";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) 
+{
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('rp', 'singh', 'rpsingh@gmail.com')";
+
+if ($conn->query($sql) === TRUE) 
+{
+  echo "New record created successfully";
+}
+else
+{
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
